3f269f6229b15f525daac99c171dc7cf60a5ba3d
Matt Jewell <m@mattjewell.co.uk>
2020-12-01 10:24:32 +0100

    part 2 complete

--- day-1/src/Day1.kt
+++ day-1/src/Day1.kt
@@ -5,25 +5,35 @@ import kotlin.test.assertNotNull
 
 class Day1(private val target: Int, private val expenseReport: List<Int>) {
 
-    fun answer() = product(expenseReport).firstOrNull { it.first + it.second == target }
+    fun answer2() = product(expenseReport).firstOrNull { it.first + it.second == target }
+    fun answer3() = triple(expenseReport).firstOrNull { it.first + it.second + it.third == target }
 
-    private fun product(list: List<Int>): Sequence<Pair<Int,Int>> = sequence {
+    private fun product(list: List<Int>): Sequence<Pair<Int, Int>> = sequence {
         list.forEachIndexed { i, first ->
-            list.reversed().subList(i, list.size -1).forEach { second ->
+            list.subList(i, list.size - 1).forEach { second ->
                 yield(first to second)
             }
         }
     }
 
+    private fun triple(list: List<Int>): Sequence<Triple<Int, Int, Int>> = sequence {
+        list.forEachIndexed { i, first ->
+            list.subList(i, list.size - 1).forEach { second ->
+                list.subList(i + 1, list.size - 1).forEach { third ->
+                    yield(Triple(first, second, third))
+                }
+            }
+        }
+    }
+
+
 }
-
-
 
 
 fun main(args: Array<String>) {
     val testReport = listOf(1721, 979, 366, 299, 675, 1456)
     val test = Day1(2020, testReport)
-    val testAnswer = test.answer()
+    val testAnswer = test.answer2()
 
     assertNotNull(testAnswer)
     assertEquals(2020, testAnswer.first + testAnswer.second)
@@ -31,12 +41,17 @@ fun main(args: Array<String>) {
     val txtReport = File("${System.getProperty("user.dir")}/day-1/src/report.txt")
             .useLines { line -> line.toList().map { it.toInt() } }
     val day1 = Day1(2020, txtReport)
-    val day1Answer = day1.answer()
+    val part1Answer = day1.answer2()
+    val part2Answer = day1.answer3()
 
-    assertNotNull(day1Answer)
-    assertEquals(2020, day1Answer.first + day1Answer.second)
+    assertNotNull(part1Answer)
+    assertEquals(2020, part1Answer.first + part1Answer.second)
+
+    assertNotNull(part2Answer)
+    assertEquals(2020, part2Answer.first + part2Answer.second + part2Answer.third)
 
 
-    println(day1Answer)
+    println("Part one: ${part1Answer.first * part1Answer.second}")
+    println("Part two: ${part2Answer.first * part2Answer.second * part2Answer.third}")
 
 }
