7de0cd03cf3599bb0a289e756f3f254afcff745c
Matt Jewell <m@mattjewell.co.uk>
2020-12-05 09:09:16 +0100

    binary solution to day 5

--- day-05/src/Day5.kt
+++ day-05/src/Day5.kt
@@ -4,40 +4,53 @@ import kotlin.test.assertEquals
 
 class Day5 {
     companion object {
-
         fun getSeat(s: String): Seat {
-            var row = 0..127
-            var col = 0..8
-            s.forEach { char ->
-                when (char) {
-                    'F' -> row = frontHalf(row)
-                    'B' -> row = backHalf(row)
-                    'R' -> col = backHalf(col)
-                    'L' -> col = frontHalf(col)
-                    else -> {
-                        TODO("Char " + char + " was unexpected")
-                    }
-                }
-            }
-            return Seat(row.first, col.first)
-        }
+        val row = s.filter { it == 'F' || it == 'B' }.replace('B', '1').replace('F', '0').toInt(2)
+            val col = s.filter { it == 'L' || it == 'R' }.replace('R', '1').replace('L', '0').toInt(2)
+            return Seat(row,col)
 
-        fun backHalf(range: IntRange): IntRange {
-            val rangeSize = range.last - range.first
-            val newRangeSize = Math.ceil(rangeSize / 2.0).toInt()
-            val intRange = (range.first + newRangeSize)..range.last
-            return intRange
         }
-
-        fun frontHalf(range: IntRange): IntRange {
-            val rangeSize = range.last - range.first
-            val newRangeSize = rangeSize / 2
-            val intRange = range.first..(range.first + newRangeSize)
-            return intRange
-        }
-
     }
 }
+
+
+// Initial solution
+//class Day5 {
+//    companion object {
+//
+//        fun getSeat(s: String): Seat {
+//            var row = 0..127
+//            var col = 0..8
+//            s.forEach { char ->
+//                when (char) {
+//                    'F' -> row = frontHalf(row)
+//                    'B' -> row = backHalf(row)
+//                    'R' -> col = backHalf(col)
+//                    'L' -> col = frontHalf(col)
+//                    else -> {
+//                        TODO("Char " + char + " was unexpected")
+//                    }
+//                }
+//            }
+//            return Seat(row.first, col.first)
+//        }
+//
+//        fun backHalf(range: IntRange): IntRange {
+//            val rangeSize = range.last - range.first
+//            val newRangeSize = Math.ceil(rangeSize / 2.0).toInt()
+//            val intRange = (range.first + newRangeSize)..range.last
+//            return intRange
+//        }
+//
+//        fun frontHalf(range: IntRange): IntRange {
+//            val rangeSize = range.last - range.first
+//            val newRangeSize = rangeSize / 2
+//            val intRange = range.first..(range.first + newRangeSize)
+//            return intRange
+//        }
+//
+//    }
+//}
 
 data class Seat(val row: Int, val col: Int) {
     val id: Int
@@ -67,7 +80,7 @@ fun main() {
 fun runTests() {
     val test = Day5()
 
-    assertEquals(0..63, Day5.frontHalf(0..127))
+//    assertEquals(0..63, Day5.frontHalf(0..127))
 
 
     assertEquals(Seat(44, 5), Day5.getSeat("FBFBBFFRLR"))
